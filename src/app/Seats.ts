edab353067251130d4c944a3bdd44efc739ca34c
baghelshivam <baghelshivam11@gmail.com>
2023-02-04 10:48:04 +0530

    Uploadin to github

---
+++ src/app/Seats.ts
@@ -0,0 +1,8 @@
+export interface Seats {    //objects containing row number
+    row : number[]
+}
+
+export interface Row {      //row with their empty seats and no.
+    Seat : number,
+    rowNo : number
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { FrontComponent } from './components/front/front.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrontComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
